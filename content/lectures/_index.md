6a4e041189b4710de689db7a90187cb9e0c2f58e
SoldiVi <viktoriasoldi@gmail.com>
2026-03-31 23:38:17 +0300

    add sections

---
+++ content/lectures/_index.md
@@ -0,0 +1,7 @@
++++
+title = "Лекції"
+type = "section"
+weight = 1
++++
+
+This is a new chapter.
--- public/searchindex.en.js
+++ public/searchindex.en.js
@@ -1,6 +1,14 @@
 var relearn_searchindex = [
   {
     "breadcrumb": "Головна",
+    "content": "This is a new chapter.",
+    "description": "This is a new chapter.",
+    "tags": [],
+    "title": "Лекції",
+    "uri": "/lectures/index.html"
+  },
+  {
+    "breadcrumb": "Головна",
     "content": "",
     "description": "",
     "tags": [],
@@ -16,11 +24,43 @@ var relearn_searchindex = [
     "uri": "/tags/index.html"
   },
   {
+    "breadcrumb": "Головна \u003e Тести",
+    "content": "Завантаження…",
+    "description": "Завантаження…",
+    "tags": [],
+    "title": "Test test",
+    "uri": "/tests/test1/test/index.html"
+  },
+  {
     "breadcrumb": "",
     "content": "Вітаю 👋 Головна сторінка освітнього проєкту Code Osvita",
     "description": "Вітаю 👋 Головна сторінка освітнього проєкту Code Osvita",
     "tags": [],
     "title": "Головна",
     "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "Головна",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Лабораторні роботи",
+    "uri": "/labs/index.html"
+  },
+  {
+    "breadcrumb": "Головна",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Про сайт",
+    "uri": "/about/index.html"
+  },
+  {
+    "breadcrumb": "Головна",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Тести",
+    "uri": "/tests/index.html"
   }
 ]
