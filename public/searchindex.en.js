1d6bbbac221b29cf6b1912f8b3635841741c91e1
SoldiVi <viktoriasoldi@gmail.com>
2026-03-31 18:12:49 +0300

    fresh start

---
+++ public/searchindex.en.js
@@ -0,0 +1,26 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "Головна",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "Головна",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "Вітаю 👋 Головна сторінка освітнього проєкту Code Osvita",
+    "description": "Вітаю 👋 Головна сторінка освітнього проєкту Code Osvita",
+    "tags": [],
+    "title": "Головна",
+    "uri": "/index.html"
+  }
+]
